074792dfb281bdde3c76e4db1e43ebc97245936d
Boris <newpostru@list.ru>
2020-07-09 17:23:52 +0300

    implement tasks: 2 3 5

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,60 @@
+class Wolf {
+    private String gender;
+    private String name;
+    private String color;
+    private int weight;
+    private int age;
+
+    // getters/setters
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+        this.age = age;
+        }
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    // behaviors
+    public void go() {
+        System.out.println("по разному");
+    }
+    public void sit() {
+        System.out.println("stop");
+    }
+    public void run() {
+        System.out.println("по разному");
+    }
+    public void voice() {
+        System.out.println("громко");
+    }
+    public void hunt() {
+        System.out.println("по разному");
+    }
+}
